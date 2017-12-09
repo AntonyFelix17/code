f17b909b6fe8edc366fe8d1523d63eddc047f7c7
AntonyFelix17 <34398192+AntonyFelix17@users.noreply.github.com>
2017-12-09 20:58:25 +0530

    Update multiply.cpp

--- multiply.cpp
+++ multiply.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-void Divide (double x, double y,double& mul) {
+void Multiply (double x, double y,double& mul) {
   mul = x * y;
 }
 int main() {
   double n1, n2, m;
   
   cin>> n1 >> n2;
-  Divide(n1, n2, m);
+  Multiply(n1, n2, m);
   cout<< n1 <<" * " << n2 << " = " << m << endl;
       
   return 0;
