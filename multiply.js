46f82c12ae356632deba9e0b8c4e8745ea8ffa6f
AntonyFelix17 <34398192+AntonyFelix17@users.noreply.github.com>
2017-12-09 21:07:53 +0530

    Create multiply.js

---
+++ multiply.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Javascript file</h2>
+
+
+<p id="demo"></p>
+
+<script>
+var x = window.prompt();
+var y = window.prompt();
+var z = x * y;
+document.getElementById("demo").innerHTML = z;
+</script>
+
+</body>
+</html>
